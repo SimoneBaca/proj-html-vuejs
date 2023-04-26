4e8069d0ee08498635c26457b3651bc0d2f41764
Simonebaca <simobaco99@gmail.com>
2023-04-26 18:02:45 +0200

    start scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "Header",
+  
+}
+</script>
+
+<template>
+    <header>
+        Avada marketing
+    </header>
+</template>
+
+
+
+<style lang="scss" scoped>
+header {
+    height: 80px;
+    background-color:lime;
+}
+</style>
