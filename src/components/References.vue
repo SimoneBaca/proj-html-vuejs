e97843c933625e72bb9ceefa881a11e27639a126
Simonebaca <simobaco99@gmail.com>
2023-04-29 15:07:20 +0200

    awards done without img

--- src/components/References.vue
+++ src/components/References.vue
@@ -2,7 +2,7 @@
     <div class="content">
         <div class="container">
             <div class="row-card">
-            <!-- Text -->
+                <!-- Text -->
                 <div class="text-area">
                     <h2 class="title-section">
                         We Boost Our Clients' Bottom Line by Optimizing Their Growth Potential.
@@ -73,6 +73,13 @@
             <!-- AWARDS -->
             <div class="awards">
                 <h2>Awards</h2>
+                <ul class="awards-list">
+                    <li v-for="(awards, index) in awards" :key="index">
+                        <img :src="awards.thumb" :alt="awards.alt">
+                        <span>{{ awards.name }}</span>
+                        <span>{{ awards.info }}</span>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -82,6 +89,32 @@
 
 export default {
     name: "References",
+    data: function () {
+        return {
+            awards: [
+                {
+                    name: 'Winner Seo Master MAGT',
+                    info: 'Smart Start Award 2017',
+                    alt: 'logo award seo master magt',
+                },
+                {
+                    name: 'Top Social Media Agencies',
+                    info: 'Next Partner 2018',
+                    alt: 'logo award top social media agencies',
+                },
+                {
+                    name: '10 Fastest Growing Abstract',
+                    info: 'Solution Providers 2019',
+                    alt: 'logo award 10 fastest growing abstract',
+                },
+                {
+                    name: 'National Excellence Agencie',
+                    info: 'Award Winner 2020',
+                    alt: 'logo award national excellence agencie',
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -107,49 +140,77 @@ export default {
         .p-section {
             padding: 30px 0;
         }
+
         img {
             border-radius: 10px;
         }
     }
+
     // INFO
     .info-text {
         display: flex;
         // justify-content: space-around;
         align-items: center;
+
         .text-area {
             width: 50%;
             padding-right: 130px;
+
             h2 {
                 font-size: 28px;
             }
+
             p {
                 font-size: 20px;
                 padding: 20px 0 80px 0;
             }
         }
+
         .info-icons {
             width: 50%;
             margin-left: 140px;
+
             .single-icon-area {
                 display: flex;
                 align-items: center;
                 padding-bottom: 40px;
+
                 img {
                     padding-right: 25px;
                 }
+
                 h2 {
                     padding-bottom: 20px;
                 }
             }
         }
     }
+
     // AWARDS
     .awards {
         padding-top: 50px;
-        
+
         h2 {
             text-align: center;
         }
+
+        .awards-list {
+            display: flex;
+
+            li {
+                display: flex;
+                flex-direction: column;
+                text-align: center;
+
+                img {
+                    width: 160px;
+                }
+
+                span {
+                    font-size: 18px;
+                    padding-bottom: 10px;
+                }
+            }
+        }
     }
-}
-</style>
+}</style>
